4f39fe1139006b575e981b8ac7c1357e7567d6c7
cjd1011 <77751497+cjd1011@users.noreply.github.com>
2022-07-18 18:20:26 -0500

    5th change

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,5 @@ st.info('Professional in Business Administration, currently working in Business 
 icon_size = 20
 
 st_button('youtube', 'https://www.youtube.com/channel/UCAdEr61O-jMY0SyzApo91iA', 'Inteligencia de Negocios Para Todos YouTube channel', icon_size)
-st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
-st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
-st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+st_button('linkedin', 'https://www.linkedin.com/in/camilo-diaz-c-21a842b6/', 'Follow me on LinkedIn', icon_size)
+st_button('cup', 'https://www.buymeacoffee.com/camilodiaz', 'Buy me a Coffee', icon_size)
